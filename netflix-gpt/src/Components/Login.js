69f07d4a39467f991a875ac388a2404a6c1bc2e3
siddhart <siddhart.2021ca103@mnnit.ac.in>
2023-10-16 14:30:16 +0530

    readme

--- netflix-gpt/src/Components/Login.js
+++ netflix-gpt/src/Components/Login.js
@@ -15,6 +15,8 @@ const Login = () => {
     //  to get this email and password, we can use state variable(as done in prev project) or reference to the input box using useRef (done in this project)
     const message=checkValidData(email.current.value,password.current.value)
     setErrorMessage(message)
+
+    // if valid procced further
   }
   return (
     <div>
@@ -27,11 +29,11 @@ const Login = () => {
 
       {/* basically what happens is whn we create a button and click it, by default onSubmit event is called, and since we dont have onSubmit in form it was refreshing the page, i.e., why we need to prevent it from performing any action, so e.preventdefault where e is event */}
       <form onSubmit={(e) => e.preventDefault()} className=' text-white absolute bg-black w-3/12 my-36 mx-auto right-0 left-0 bg-opacity-80 rounded-md'>
-        <h1 className='px-4 font-bold text-3xl py-6 '>{isSignIn ? "Sign In" : "Sign Up"}</h1>
+        <h1 className=' mx-10 px-4 font-bold text-3xl py-6 '>{isSignIn ? "Sign In" : "Sign Up"}</h1>
         {!isSignIn && (<input className="mx-14 my-2 w-8/12 rounded-sm px-4 bg-slate-500 p-1" type="text" placeholder='FullName' />)}
         <input className="mx-14 my-2 w-8/12 rounded-sm px-4 bg-slate-500 p-1" ref={email} type="text" placeholder='Email Address' />
         <input className="mx-14 my-2 w-8/12 rounded-sm px-4 bg-slate-500 p-1" ref={password} type="password" placeholder='Password' />
-        <p className='text-red-600'>{errorMessage}</p>
+        <p className='text-red-600 font-bold font-sans py-2 my-2 mx-14 px-4'>{errorMessage}</p>
         <button className="my-2 mx-14 p-1 w-8/12 rounded-sm bg-red-700 px-4" onClick={handleButtonClick}>{isSignIn ? "Sign In" : "Sign Up"}</button>
         <p onClick={handleSignIn} className='cursor-pointer py-8 my-2 mx-14 px-4'>{isSignIn ? "New to Netflix? Sign Up Now" : "Already a member?Sign In Now"}</p>
       </form>
